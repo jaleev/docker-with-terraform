f824697364710fc20205a7a48e1157d6b4a649d9
jaleev <devops@manrala.com>
2024-09-08 18:10:42 -0400

    Created the script for full IP output

--- docker-terraform/outputs.tf
+++ docker-terraform/outputs.tf
@@ -1,7 +1,15 @@
-output "IP_Address" {
-  value = docker_container.nodered-container.network_data[0].ip_address
-}
+# output "IP_Address" {
+#   value = docker_container.nodered-container.network_data[0].ip_address
+# }
+
+# output "Ports" {
+#   value = docker_container.nodered-container.ports[0].external
+# }
 
 output "ContainerName" {
   value = docker_container.nodered-container.name
+}
+
+output "Full_IP" {
+  value = join(":", [docker_container.nodered-container.network_data[0].ip_address, docker_container.nodered-container.ports[0].external])
 }
